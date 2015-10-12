8ebd1821d6b36c57fdc05767b882b22d71e701cb
Jerry <jerry21yan@gmail.com>
2015-10-11 18:22:40 -0700

    reworked Eulersucksprime

--- Eulersucksprime.java
+++ Eulersucksprime.java
@@ -1,66 +1,43 @@
+import java.util.*;
 
-public class Eulersucksprime
-{
-	public static void main(String []args)
-	{
-		boolean prime = true;
-		int number = 0;
-		boolean numberPrime = true;
-		
-		//make prime numbers
-		for(int x = 2; x < 1000000; x++)
-		{
-			for(int y = 2; y < x; y++)
-			{
-
-				if (x % y == 0)
-				{
-					prime = false;
-				}
+public class Eulersucksprime {
+	//checks if n is prime
+	public static boolean isPrime(int n) {
+		for (int i = 2; 2 * i < n; i++) {
+			if (n % i == 0) {
+				return false;
 			}
-			//if the generated number is prime
-			if (prime == true)
-			{	
-				
-				//add the prime numbers to each other
-				number += x;			
-
-				//System.out.println(number);
-			}	
-				
-			//check if the sum is prime
-			for (int z = 2; z < number; z++)
-			{
-				if (number % z == 0)
-				{
-					numberPrime = false;	
-				}
-				//if the sum is prime
-				if (numberPrime = true)
-				{
-					//check if number is under 1000000 and over 900000
-					if (number > 900000)
-					{
-					
-						if (number < 1000000)
-						{
-							//print number 
-							System.out.println(number);
-						}
-						else
-						{
-							System.exit(0);
-						}
-					
-					}
-
-				}
-			}			
-		
-		prime = true;
-		
 		}
-	
+		return true;
 	}
 
+	public static void main(String[] args) {
+		int number = 0;
+		//list of prime numbers
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+
+		//makes the list
+		for (int x = 2; x < 1000000; x++) {
+			if(Eulersucksprime.isPrime(x)){
+				primes.add(primes.size(), x);
+			}
+		}
+		while(true){
+			//reset variable each time so it doesn't go out of bounds
+			number = 0;
+			//adds all the prime numbers
+			for(int i = 0; i < primes.size(); i++){
+				number += primes.get(i);
+			}
+			//checks if it meats the parameters
+			if(number < 1000000 && Eulersucksprime.isPrime(number) && number > 0){
+				System.out.println(number);
+				break;
+			}
+			//removes last item from the list, repeats
+			primes.remove(primes.size() - 1);
+		}
+
+	}
 }
+
